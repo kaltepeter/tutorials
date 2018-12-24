8dcdfc1c2aec2dc57bcec7c0cad664966bb4494c
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-12-24 10:10:01 -0600

    adding first comp

---
+++ pluralsight-tdd-jest/src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>This is the header.</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
