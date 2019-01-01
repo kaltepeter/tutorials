aa99133607f2245df60fd03e84086de336ac096c
Kayla Altepeter <kayla@kaylaaltepeter.com>
2019-01-01 11:52:59 -0600

    rearrange for modules

--- pluralsight-typescript-in-depth/LibraryManager/lib/utilityFunctions.ts
+++ pluralsight-typescript-in-depth/LibraryManager/lib/utilityFunctions.ts
@@ -1,19 +1,15 @@
-namespace Utility {
-    export namespace Fees {
-        export const CalculateLateFee = (daysLate: number): number => {
-            return daysLate * .25;
-        }
-    }
+export const CalculateLateFee = (daysLate: number): number => {
+    return daysLate * .25;
+}
 
-    export const MaxBooksAllowed = (age: number): number => {
-        if (age < 12) {
-            return 3;
-        } else {
-            return 10;
-        }
+export const MaxBooksAllowed = (age: number): number => {
+    if (age < 12) {
+        return 3;
+    } else {
+        return 10;
     }
+}
 
-    const privateFunc = (): void => {
-        console.log('this is a private function.')
-    }
+const privateFunc = (): void => {
+    console.log('this is a private function.')
 }
