f9d75db56c9c8264860719550eaec84a3c029a6e
Kayla Altepeter <kayla@kaylaaltepeter.com>
2019-04-06 16:17:32 -0500

    initial singleton code

---
+++ design-patterns-in-typescript/creation/app.ts
@@ -0,0 +1,15 @@
+import { StatsTracker } from "./stats-tracker";
+
+let tracker = StatsTracker.instance;
+tracker.widgetViews = 90;
+tracker.buttonClicks = 64;
+tracker.facebookShares = 20;
+tracker.twitterShares = 30;
+
+console.log("TCL: tracker.widgetViews", tracker.widgetViews);
+console.log("TCL: tracker.buttonClicks", tracker.buttonClicks);
+console.log("TCL: tracker.facebookShares", tracker.facebookShares);
+console.log("TCL: tracker.twitterShares", tracker.twitterShares);
+
+
+
