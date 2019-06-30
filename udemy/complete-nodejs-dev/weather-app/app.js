eb4e45638b93a4315d70139d7186d43477eff43c
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 09:29:46 -0500

    6.29

---
+++ udemy/complete-nodejs-dev/weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('starting');
+
+setTimeout(() => {
+    console.log('2 second timer');
+}, 2000);
+
+setTimeout(() => {
+    console.log('0 second timer');
+}, 0);
+
+console.log('stopping');
