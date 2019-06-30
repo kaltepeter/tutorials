bdff11ea531548b98e93d4edf35866e721fb690e
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 10:16:27 -0500

    6.32

---
+++ udemy/complete-nodejs-dev/weather-app/README.md
@@ -0,0 +1,11 @@
+# weather-app
+
+## setup
+
+* Get darksky account: https://darksky.net/dev
+* Set darksky env vars:
+
+```bash
+export DARKSKY_API_KEY='<mykey>'
+export DARKSKY_LOC='<mylonglat>'
+```
--- udemy/complete-nodejs-dev/weather-app/app.js
+++ udemy/complete-nodejs-dev/weather-app/app.js
@@ -4,7 +4,8 @@ const darkSkyLoc = process.env.DARKSKY_LOC;
 
 const url = `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyLoc}`;
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data);
+request({url: url, json: true}, (error, response) => {
+    const currently = response.body.currently;
+    const today = response.body.daily.data[0];
+    console.log(`${today.summary} It is currently ${currently.temperature} out. There is a ${currently.precipProbability}% chance of rain.`);
 });
