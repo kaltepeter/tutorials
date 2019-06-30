9b11f1de2a061a5779415fe089933b3420dfc767
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-29 23:22:35 -0500

    4.17

--- udemy/complete-nodejs-dev/notes-app/app.js
+++ udemy/complete-nodejs-dev/notes-app/app.js
@@ -9,7 +9,22 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: () => { console.log('adding a new note') }
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note content',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => { 
+        console.log(`Title: ${argv.title}`);
+        console.log(`Body: ${argv.body}`);
+    }
 });
 
 yargs.command({
@@ -36,4 +51,6 @@ yargs.command({
     }
 });
 
-console.log(yargs.argv);
+yargs.parse();
+
+// console.log(yargs.argv);
