e8f0d5bfa5810db59d78bebb1c9a720f1c446a9b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 11:16:53 -0500

    6.35

---
+++ udemy/complete-nodejs-dev/playground/4-callbacks.js
@@ -0,0 +1,32 @@
+setTimeout(() => {
+    console.log('Two seconds are up!');
+}, 2000);
+
+const names = ['Andrew', 'Jon', 'Jess'];
+const shortNames = names.filter((name) => {
+    return name.length <= 4;
+});
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        };
+        callback(data);
+    }, 2000);
+};
+
+// geocode('Philidelphia', (data) => {
+//     console.log(data);
+// });
+
+const add = (x, y, callback) => {
+    setTimeout(() => {
+        callback(x + y);
+    }, 2000);
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+});
