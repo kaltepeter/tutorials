96d0f9cfe630a6b09846b60a4ac6049c237a5857
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 12:43:18 -0500

    6.40

--- udemy/complete-nodejs-dev/weather-app/utils/forecast.js
+++ udemy/complete-nodejs-dev/weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const darkSkyApiKey = process.env.DARKSKY_API_KEY;
 
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/${darkSkyApiKey}/${latitude},${longitude}`;
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, { body: {error:apiError, daily, currently} }) => {
         if (error) {
             callback('Unable to connect to weather service!');
-        } else if (response.body.error) {
+        } else if (apiError) {
             callback('Unable to find location for forecast!');
         } else {
-            const currently = response.body.currently;
-            const today = response.body.daily.data[0];
-            callback(undefined, `${today.summary} It is currently ${currently.temperature} out. There is a ${currently.precipProbability}% chance of rain.`);
+            const {temperature, precipProbability} = currently;
+            const today = daily.data[0];
+            callback(undefined, `${today.summary} It is currently ${temperature} out. There is a ${Math.round(precipProbability * 100)}% chance of rain.`);
         }
     });
 };
--- udemy/complete-nodejs-dev/weather-app/utils/geocode.js
+++ udemy/complete-nodejs-dev/weather-app/utils/geocode.js
@@ -3,14 +3,14 @@ const mapboxApiKey = process.env.MAPBOX_API_KEY;
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxApiKey}&limit=1`;
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!');
-        } else if (response.body.features.length <= 0) {
+        } else if (body.features.length <= 0) {
             callback(`Unable to find location. Try another search.`);
         } else {
-            const [longitude, latitude] = response.body.features[0].center;
-            const location = response.body.features[0].place_name;
+            const [longitude, latitude] = body.features[0].center;
+            const location = body.features[0].place_name;
             callback(undefined, {latitude, longitude, location});
         }
     });
