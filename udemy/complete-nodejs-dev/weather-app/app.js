75cb146d8b06919ff797a5b1cd2dea54900180f5
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 11:28:28 -0500

    6.36

--- udemy/complete-nodejs-dev/weather-app/app.js
+++ udemy/complete-nodejs-dev/weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
+const geocode = require('./utils/geocode');
 const darkSkyApiKey = process.env.DARKSKY_API_KEY;
 const darkSkyLoc = process.env.DARKSKY_LOC;
-const mapboxApiKey = process.env.MAPBOX_API_KEY;
 
 const url = `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyLoc}`;
 
@@ -17,14 +17,7 @@ const url = `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyLoc}`;
 //     }
 // });
 
-const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angelos.json?access_token=${mapboxApiKey}&limit=1`;
-request({url: geocodeUrl, json: true}, (error, response) => {
-    if (error) {
-        console.log(`Unable to connect to location services!`);
-    } else if (response.body.features.length <= 0) {
-        console.log(`Unable to find location. Try another search.`);
-    } else {
-        const [latitude, longitude] = response.body.features[0].center;
-        console.log(latitude, longitude);
-    }
+geocode('Boston', (error, data) => {
+    console.log(`Error: `, error);
+    console.log(`Data: `, data);
 });
