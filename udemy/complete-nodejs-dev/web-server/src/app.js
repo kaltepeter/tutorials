497ec7fe3a1457a1e4b92d78139461da159ba0ed
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 14:04:39 -0500

    7.47

--- udemy/complete-nodejs-dev/web-server/src/app.js
+++ udemy/complete-nodejs-dev/web-server/src/app.js
@@ -5,7 +5,29 @@ const publicDirectoryPath = path.join(__dirname, '..', 'public');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'Help message'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
