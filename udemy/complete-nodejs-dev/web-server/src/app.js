2e615d5126e505017993387cbc7b30836f0c25a2
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 13:11:09 -0500

    7.44

--- udemy/complete-nodejs-dev/web-server/src/app.js
+++ udemy/complete-nodejs-dev/web-server/src/app.js
@@ -2,19 +2,29 @@ const express = require('express');
 
 const app = express();
 app.get('', (req, res) => {
-    res.send('Hello express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send([
+        {
+            name: 'Andrew',
+        },
+        {
+            name: 'Sarah'
+        }
+    ]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About');
+    res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather');
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philidelphia'
+    });
 });
 
 app.listen(3000, () => {
