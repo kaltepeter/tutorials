c54c84836fe3228cea4ca255a3d0caf81f1e6fbd
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-29 23:16:02 -0500

    4.16

--- udemy/complete-nodejs-dev/notes-app/app.js
+++ udemy/complete-nodejs-dev/notes-app/app.js
@@ -1,10 +1,39 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+// customize version
+yargs.version('1.1.0');
 
-if (command === 'add') {
-    console.log('adding note!');
-} else if (command == 'remove') {
-    console.log('removing notes');
-}
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => { console.log('adding a new note') }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('removing the note');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: () => {
+        console.log('Listing out all notes.');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read note', 
+    handler: () => {
+        console.log('Reading a note.');
+    }
+});
+
+console.log(yargs.argv);
