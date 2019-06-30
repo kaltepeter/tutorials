051475587d9dd5e88305c631be9f4675524cb164
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 00:34:43 -0500

    4.24

--- udemy/complete-nodejs-dev/notes-app/notes.js
+++ udemy/complete-nodejs-dev/notes-app/notes.js
@@ -7,9 +7,10 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter((note) => note.title === title );
+    // const duplicateNotes = notes.filter((note) => note.title === title );
+    const duplicateNote = notes.find((note) => note.title === title);
 
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -56,9 +57,21 @@ const listNotes = () => {
     return notes;
 };
 
+const readNote = (title) => {
+    const notes = loadNotes();
+    const note = notes.find((note) => note.title === title);
+    if (note) {
+        console.log(chalk.inverse(note.title));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red.inverse(`No note found.`));
+    }
+};
+
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 };
