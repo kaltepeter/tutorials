27d7d14ee93a68ec4e882420a5d11eecfe122fde
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-06-30 10:50:47 -0500

    6.34

--- udemy/complete-nodejs-dev/weather-app/README.md
+++ udemy/complete-nodejs-dev/weather-app/README.md
@@ -8,4 +8,5 @@
 ```bash
 export DARKSKY_API_KEY='<mykey>'
 export DARKSKY_LOC='<mylonglat>'
+export MAPBOX_API_KEY='<mymapboxkey>'
 ```
--- udemy/complete-nodejs-dev/weather-app/app.js
+++ udemy/complete-nodejs-dev/weather-app/app.js
@@ -1,11 +1,30 @@
 const request = require('request');
 const darkSkyApiKey = process.env.DARKSKY_API_KEY;
 const darkSkyLoc = process.env.DARKSKY_LOC;
+const mapboxApiKey = process.env.MAPBOX_API_KEY;
 
 const url = `https://api.darksky.net/forecast/${darkSkyApiKey}/${darkSkyLoc}`;
 
-request({url: url, json: true}, (error, response) => {
-    const currently = response.body.currently;
-    const today = response.body.daily.data[0];
-    console.log(`${today.summary} It is currently ${currently.temperature} out. There is a ${currently.precipProbability}% chance of rain.`);
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location');
+//     } else {
+//         const currently = response.body.currently;
+//         const today = response.body.daily.data[0];
+//         console.log(`${today.summary} It is currently ${currently.temperature} out. There is a ${currently.precipProbability}% chance of rain.`);
+//     }
+// });
+
+const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angelos.json?access_token=${mapboxApiKey}&limit=1`;
+request({url: geocodeUrl, json: true}, (error, response) => {
+    if (error) {
+        console.log(`Unable to connect to location services!`);
+    } else if (response.body.features.length <= 0) {
+        console.log(`Unable to find location. Try another search.`);
+    } else {
+        const [latitude, longitude] = response.body.features[0].center;
+        console.log(latitude, longitude);
+    }
 });
