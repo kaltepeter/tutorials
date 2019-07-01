fa139f70a1ac07e75d33110c1e130aaf0ccfb973
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-01 00:08:13 -0500

    10.78

--- udemy/complete-nodejs-dev/task-manager/mongodb.js
+++ udemy/complete-nodejs-dev/task-manager/mongodb.js
@@ -3,10 +3,6 @@ const {MongoClient, ObjectID} = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id);
-console.log(id.getTimestamp());
-
 MongoClient.connect(connectionURL, 
     {
     useNewUrlParser: true
@@ -17,49 +13,30 @@ MongoClient.connect(connectionURL,
         }
 
         const db = client.db(databaseName);
-        // db.collection('users').insertOne({
-        //     name: 'Kayla',
-        //     age: 34
-        // }, (error, result) => {
-        //     if (error) {
-        //         return console.error('Unable to insert user');
-        //     }
+        db.collection('users').findOne({_id: new ObjectID('5d1988feb14bc65bae982343')}, (error, user) => {
+            if (error) {
+                return console.error('Unable to fetch.');
+            }
+            console.log(user);
+        });
 
-        //     console.log(result.ops);
-        // });
-        // db.collection('users').insertMany([
-        //     {
-        //         name: 'Jon',
-        //         age: 28
-        //     },
-        //     {
-        //         name: 'Gunther',
-        //         age: 27
-        //     }
-        // ], (error, result) => {
-        //     if (error) {
-        //         return console.error('Unable to insert user');
-        //     }
-        //     console.log(result.ops);
-        // });
+        db.collection('users').find({age: 27}).count((error, users) => {
+            if (error) {
+                return console.error('Unable to fetch.');
+            }
+            console.log(users);
+        });
 
-        // db.collection('tasks').insertMany([
-        //     {
-        //         description: 'Make lunch',
-        //         completed: false
-        //     },
-        //     {
-        //         description: 'Pack bags',
-        //         completed: false
-        //     },
-        //     {
-        //         description: 'Make coffee',
-        //         completed: false
-        //     },
-        // ], (error, result) => {
-        //     if (error) {
-        //         return console.error('Unable to insert user');
-        //     }
-        //     console.log(result.ops);
-        // });
+        db.collection('tasks').findOne({_id: new ObjectID('5d198b58101e6568b3bdde16')}, (error, task) => {
+            if (error) {
+                return console.error('Unable to fetch task.');
+            }
+            console.log(task);
+        });
+        db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+            if (error) {
+                return console.error('Unable to fetch.');
+            }
+            console.log(tasks);
+        });
     });
