aa1eafabc738349b8e26780a6dbf72647a5af943
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-27 23:41:48 -0500

    module-8: prompt

--- pluralsight/pluralsight-react-flux/ps-flux/src/components/ManageCoursePage.js
+++ pluralsight/pluralsight-react-flux/ps-flux/src/components/ManageCoursePage.js
@@ -4,6 +4,7 @@ const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Course</h2>
+      {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
       {props.match.params.slug}
     </>
   );
