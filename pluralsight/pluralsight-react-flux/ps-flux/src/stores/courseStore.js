f0d6afcf134f4ee0497dac4bbd83acf2c2bf1177
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-28 09:55:33 -0500

    module-10: adding store

---
+++ pluralsight/pluralsight-react-flux/ps-flux/src/stores/courseStore.js
@@ -0,0 +1,28 @@
+import { EventEmitter } from "events";
+
+import Dispatcher from "../appDispatcher";
+
+const CHANGE_EVENT = "change";
+
+class CourseStore extends EventEmitter {
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+}
+
+const store = new CourseStore();
+
+Dispatcher.register(action => {
+  switch (action.actionType) {
+  }
+});
+
+export default store;
