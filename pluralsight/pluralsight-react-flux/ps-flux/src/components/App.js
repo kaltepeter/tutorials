4843eaf8cce12a7499717a1fe163755d6040d9a1
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-27 23:04:47 -0500

    module-8: use Link

--- pluralsight/pluralsight-react-flux/ps-flux/src/components/App.js
+++ pluralsight/pluralsight-react-flux/ps-flux/src/components/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-import CoursesPage from './CoursesPage';
-import Header from './common/Header';
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
+import Header from "./common/Header";
 
 const App = () => {
-    const getPage = () => {
-        const route = window.location.pathname;
-        if (route === '/about') return <AboutPage />;
-        if (route === '/courses') return <CoursesPage />;
-        return <HomePage />;
-    }
-    return (
-        <div className="container-fluid">
-            <Header />
-            {getPage()}
-        </div>
-    );
-}
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  );
+};
 
 export default App;
