fc5a29752202eb8a28572184be59082b3f9d26c3
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-28 09:59:34 -0500

    module-10: implement store

--- pluralsight/pluralsight-react-flux/ps-flux/src/stores/courseStore.js
+++ pluralsight/pluralsight-react-flux/ps-flux/src/stores/courseStore.js
@@ -1,8 +1,10 @@
 import { EventEmitter } from "events";
 
 import Dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
+let _courses = [];
 
 class CourseStore extends EventEmitter {
   addChangeListener(callback) {
@@ -16,12 +18,26 @@ class CourseStore extends EventEmitter {
   emitChange() {
     this.emit(CHANGE_EVENT);
   }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCourseBySlug(slug) {
+    return _courses.find(course => course.slug === slug);
+  }
 }
 
 const store = new CourseStore();
 
 Dispatcher.register(action => {
   switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    default:
+    //   nothing to do here
   }
 });
 
