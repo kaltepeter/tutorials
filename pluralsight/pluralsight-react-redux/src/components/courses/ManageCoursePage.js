86ca6cdb79e388f756f950d546a3bb5019ec4b30
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-28 23:39:46 -0500

    module-11: convert to hooks

--- pluralsight/pluralsight-react-redux/src/components/courses/ManageCoursePage.js
+++ pluralsight/pluralsight-react-redux/src/components/courses/ManageCoursePage.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+const ManageCoursePage = ({ courses, authors, loadAuthors, loadCourses }) => {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
         alert(`Loading courses failed ${error}`);
@@ -18,16 +17,14 @@ class ManageCoursePage extends React.Component {
         alert(`Loading authors failed ${error}`);
       });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
+};
 
 ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
