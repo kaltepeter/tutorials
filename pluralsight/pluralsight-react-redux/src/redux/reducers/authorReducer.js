df3de8b1580698a2ead27db108e3a8100a0d8fdc
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-28 23:08:38 -0500

    module-10: adding initial state

--- pluralsight/pluralsight-react-redux/src/redux/reducers/authorReducer.js
+++ pluralsight/pluralsight-react-redux/src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-const authorReducer = (state = [], action) => {
+const authorReducer = (state = initialState.authors, action) => {
   switch (action.type) {
     case types.LOAD_AUTHORS_SUCCESS:
       return action.authors;
--- pluralsight/pluralsight-react-redux/src/redux/reducers/courseReducer.js
+++ pluralsight/pluralsight-react-redux/src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-const courseReducer = (state = [], action) => {
+const courseReducer = (state = initialState.courses, action) => {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.course }];
