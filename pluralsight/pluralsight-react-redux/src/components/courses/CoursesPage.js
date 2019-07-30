1bb3c0b31d1947719e498b18b1579adab1632783
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-30 18:55:01 -0500

    module-12: async/await

--- pluralsight/pluralsight-react-redux/src/components/courses/CoursesPage.js
+++ pluralsight/pluralsight-react-redux/src/components/courses/CoursesPage.js
@@ -29,11 +29,13 @@ class CoursesPage extends React.Component {
     }
   }
 
-  handleDeleteCourse = course => {
+  handleDeleteCourse = async course => {
     toast.success("Course deleted.");
-    this.props.actions.deleteCourse(course).catch(error => {
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
       toast.error(`Delete failed. ${error.message}`, { autoClose: false });
-    });
+    }
   };
 
   render() {
