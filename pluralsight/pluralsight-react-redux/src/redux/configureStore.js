4555434a108729db1f7b9223657f4e2e6f9e013f
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-07-30 20:32:14 -0500

    module-15: prod builds

--- pluralsight/pluralsight-react-redux/src/redux/configureStore.js
+++ pluralsight/pluralsight-react-redux/src/redux/configureStore.js
@@ -1,16 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-const configureStore = initialState => {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
-};
-
-export default configureStore;
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
+}
---
+++ pluralsight/pluralsight-react-redux/src/redux/configureStore.prod.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const configureStore = initialState => {
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+};
+
+export default configureStore;
