3b12e86642c4e0eb592fc0e4c5eaa92f7f57838a
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-10 12:08:59 -0500

    5: adding apis

--- nrwl/nx-tutorials/apps/api/src/main.ts
+++ nrwl/nx-tutorials/apps/api/src/main.ts
@@ -4,12 +4,15 @@
  **/
 
 import * as express from 'express';
+import { addTodoRoutes } from './app/todos';
 
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
+
+addTodoRoutes(app);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
