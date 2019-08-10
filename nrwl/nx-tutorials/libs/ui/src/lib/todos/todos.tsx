30cf8dd5defe981db51619fd5f82d4d05a604c1f
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-10 12:56:39 -0500

    10: running commands

--- nrwl/nx-tutorials/libs/ui/src/lib/todos/todos.tsx
+++ nrwl/nx-tutorials/libs/ui/src/lib/todos/todos.tsx
@@ -12,7 +12,7 @@ export const Todos = (props: TodosProps) => {
   return (
     <ul>
       {props.todos.map(t => (
-        <li className={'todo'}>{t.title}</li>
+        <li className={'todo'}>{t.title} !!</li>
       ))}
     </ul>
   );
