3e987739356e346aa6a19c744ff949261defe341
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 11:05:17 -0500

    11-83: setup mongoose

---
+++ udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: "String"
+  },
+  age: {
+    type: "Number"
+  }
+});
+
+const me = new User({ name: "Kayla", age: 30 });
+
+me.save()
+  .then(result => console.log(result))
+  .catch(error => console.error("Error! ", error));
