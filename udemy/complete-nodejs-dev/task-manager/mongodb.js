5e29ebe446f9157a048d5b3f3d28ae714bb58337
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 10:30:20 -0500

    10-80: updating documents

--- udemy/complete-nodejs-dev/task-manager/mongodb.js
+++ udemy/complete-nodejs-dev/task-manager/mongodb.js
@@ -1,42 +1,50 @@
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require("mongodb");
 
-const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'task-manager';
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, 
-    {
+MongoClient.connect(
+  connectionURL,
+  {
     useNewUrlParser: true
-    }, 
-    (error, client) => {
-        if (error) {
-            return console.error('Unable to connect to the database!');
+  },
+  (error, client) => {
+    if (error) {
+      return console.error("Unable to connect to the database!");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: false
+        },
+        {
+          $set: {
+            completed: true
+          }
         }
+      )
+      .then(result => console.log(result))
+      .catch(error => console.error(error));
 
-        const db = client.db(databaseName);
-        db.collection('users').findOne({_id: new ObjectID('5d1988feb14bc65bae982343')}, (error, user) => {
-            if (error) {
-                return console.error('Unable to fetch.');
-            }
-            console.log(user);
-        });
-
-        db.collection('users').find({age: 27}).count((error, users) => {
-            if (error) {
-                return console.error('Unable to fetch.');
-            }
-            console.log(users);
-        });
-
-        db.collection('tasks').findOne({_id: new ObjectID('5d198b58101e6568b3bdde16')}, (error, task) => {
-            if (error) {
-                return console.error('Unable to fetch task.');
-            }
-            console.log(task);
-        });
-        db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
-            if (error) {
-                return console.error('Unable to fetch.');
-            }
-            console.log(tasks);
-        });
-    });
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("5d1988feb14bc65bae982343")
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+  }
+);
