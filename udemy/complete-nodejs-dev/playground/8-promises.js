b18920894115d7d626103b9f050d4fed95c66ffd
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 10:07:11 -0500

    10-79: promises

--- udemy/complete-nodejs-dev/playground/8-promises.js
+++ udemy/complete-nodejs-dev/playground/8-promises.js
@@ -1,14 +1,23 @@
 const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject('This is an error.');
-        // resolve([7, 4, 1]);
-    }, 2000);
+  setTimeout(() => {
+    reject("Things went wrong!");
+    // resolve([7, 4, 1]);
+    reject("New error");
+  }, 2000);
 });
 
 doWorkPromise
-    .then((result) => {
-        console.log(`Success! ${result}`);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+  .then(result => {
+    console.log(`Success! ${result}`);
+  })
+  .catch(error => {
+    console.error("Error!", error);
+  });
+
+//
+//                              fulfilled
+//                             /
+// Promise      -- pending -->
+//                             \
+//                              rejected
+//
