04af438cdd84f8428a584144854d5ab61dc774cc
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 23:05:17 -0500

    11-86: validation part II

--- udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
+++ udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
@@ -31,25 +31,44 @@ const User = mongoose.model("User", {
         throw new Error("Age must be a positive number");
       }
     }
+  },
+  password: {
+    type: "String",
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot contain 'password'");
+      }
+    }
   }
 });
 
-const me = new User({ name: "     Andrew   ", email: "MYEMAIL@MEAD.IO" });
+// const me = new User({
+//   name: "     Andrew   ",
+//   email: "MYEMAIL@MEAD.IO",
+//   password: "mypw123"
+// });
 
-me.save()
-  .then(result => console.log(result))
-  .catch(error => console.error("Error! ", error));
+// me.save()
+//   .then(result => console.log(result))
+//   .catch(error => console.error("Error! ", error));
 
 const Task = mongoose.model("Task", {
   description: {
-    type: "String"
+    type: "String",
+    required: true,
+    trim: true
   },
   completed: {
-    type: "Boolean"
+    type: "Boolean",
+    required: false,
+    default: false
   }
 });
 
-new Task({ description: "Pack a lunch", completed: false })
+new Task({ description: "Pack a lunch" })
   .save()
   .then(result => console.log(result))
   .catch(error => console.error(error));
