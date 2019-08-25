210841659384c617304f8e0d2287e65e2a25324a
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 09:18:31 -0500

    11-89: resource endpoints part I

--- udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
+++ udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
@@ -6,55 +6,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useCreateIndex: true
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: "String",
-    required: true,
-    trim: true
-  },
-  email: {
-    type: "String",
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    }
-  },
-  age: {
-    type: "Number",
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    }
-  },
-  password: {
-    type: "String",
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password cannot contain 'password'");
-      }
-    }
-  }
-});
-
-// const me = new User({
-//   name: "     Andrew   ",
-//   email: "MYEMAIL@MEAD.IO",
-//   password: "mypw123"
-// });
-
-// me.save()
-//   .then(result => console.log(result))
-//   .catch(error => console.error("Error! ", error));
-
 const Task = mongoose.model("Task", {
   description: {
     type: "String",
