9ec45a3b3f329615ef911c3091e6a5001e1cead5
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 10:18:27 -0500

    11-96: async/await part II

--- udemy/complete-nodejs-dev/task-manager/playground/promise-chaining.js
+++ udemy/complete-nodejs-dev/task-manager/playground/promise-chaining.js
@@ -1,10 +1,20 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("5d620864797ec54f39e0703e", { age: 1 })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then(result => console.log(result))
+// User.findByIdAndUpdate("5d620864797ec54f39e0703e", { age: 1 })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err));
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("5d620864797ec54f39e0703e", 2)
+  .then(count => console.log(count))
   .catch(err => console.error(err));
