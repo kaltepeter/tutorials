236a20689f66732aae04af24319d8089a28f551b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 09:26:21 -0500

    11-90: create resource endpoints part II

--- udemy/complete-nodejs-dev/task-manager/src/index.js
+++ udemy/complete-nodejs-dev/task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +14,19 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch(err => {
       res.status(400).send(err);
     });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => res.status(201).send(task))
+    .catch(err => res.status(400).send(err));
 });
 
 app.listen(port, () => {
