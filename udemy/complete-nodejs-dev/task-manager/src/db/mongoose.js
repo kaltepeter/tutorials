c08b65749145eafb1ac206c13acdea62d25faa55
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-24 22:53:29 -0500

    11-85: data validation part I

--- udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
+++ udemy/complete-nodejs-dev/task-manager/src/db/mongoose.js
@@ -1,24 +1,44 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: "String"
-//   },
-//   age: {
-//     type: "Number"
-//   }
-// });
+const User = mongoose.model("User", {
+  name: {
+    type: "String",
+    required: true,
+    trim: true
+  },
+  email: {
+    type: "String",
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  age: {
+    type: "Number",
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
+  }
+});
 
-// const me = new User({ name: "Kayla", age: 30 });
+const me = new User({ name: "     Andrew   ", email: "MYEMAIL@MEAD.IO" });
 
-// me.save()
-//   .then(result => console.log(result))
-//   .catch(error => console.error("Error! ", error));
+me.save()
+  .then(result => console.log(result))
+  .catch(error => console.error("Error! ", error));
 
 const Task = mongoose.model("Task", {
   description: {
