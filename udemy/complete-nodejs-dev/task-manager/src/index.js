e9dd43b1abbdb87a5d12a5e6c0b6b56a69a3c958
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 20:03:29 -0500

    11-98: updating endpoints part I

--- udemy/complete-nodejs-dev/task-manager/src/index.js
+++ udemy/complete-nodejs-dev/task-manager/src/index.js
@@ -41,6 +41,31 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
