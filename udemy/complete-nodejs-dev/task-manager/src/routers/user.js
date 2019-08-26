046a269737a63737d330b4423ed29b639463aa87
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 23:33:49 -0500

    12-113: authenticating user requests

--- udemy/complete-nodejs-dev/task-manager/src/routers/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/routers/user.js
@@ -20,20 +20,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(500).send();
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every(update =>
@@ -45,27 +32,26 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
+    updates.forEach(update => (req.user[update] = req.body[update]));
 
-    updates.forEach(update => (user[update] = req.body[update]));
-
-    await user.save();
-    if (!user) {
+    await req.user.save();
+    if (!req.user) {
       return res.status(404).send();
     }
-    res.send(user);
+    res.send(req.user);
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    // const user = await User.findByIdAndDelete(req.user._id);
+    // if (!user) {
+    //   return res.status(404).send();
+    // }
+    await req.user.remove();
+    res.send(req.user);
   } catch (err) {
     res.status(500).send(err);
   }
