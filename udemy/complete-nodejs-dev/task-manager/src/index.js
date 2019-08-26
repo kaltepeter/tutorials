d703bd8f2d1b244fbbf40e72f7cc464842ab6af9
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 21:35:54 -0500

    12-106: json web tokens

--- udemy/complete-nodejs-dev/task-manager/src/index.js
+++ udemy/complete-nodejs-dev/task-manager/src/index.js
@@ -15,15 +15,16 @@ app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const myFunction = async () => {
-  const password = "Red12345!";
-  const hashedPassword = await bcrypt.hash(password, 8);
-  console.log(password);
-  console.log(hashedPassword);
-  const isMatch = await bcrypt.compare("red12345!", hashedPassword);
-  console.log(isMatch);
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
+    expiresIn: "7 days"
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, "thisismynewcourse");
+  console.log(data);
 };
 
 myFunction();
