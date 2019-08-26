94932418422d445e63cfe6a6637c6f6b0e149d0c
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-25 21:45:20 -0500

    12-107: generating auth tokens

--- udemy/complete-nodejs-dev/task-manager/src/models/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const validator = require("validator");
 
@@ -40,8 +41,24 @@ const userSchema = new mongoose.Schema({
         throw new Error("Password cannot contain 'password'");
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: "String",
+        required: true
+      }
+    }
+  ]
 });
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user.id.toString() }, "thisismynewcourse");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
--- udemy/complete-nodejs-dev/task-manager/src/routers/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/routers/user.js
@@ -8,7 +8,8 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
@@ -80,7 +81,8 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (err) {
     res.status(400).send();
   }
