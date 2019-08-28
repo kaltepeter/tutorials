248ce23cde901d0e5adc62d289b31caa2f145138
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 07:20:09 -0500

    13-123: adding upload for avatar

--- udemy/complete-nodejs-dev/task-manager/src/index.js
+++ udemy/complete-nodejs-dev/task-manager/src/index.js
@@ -7,6 +7,14 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require("multer");
+const upload = multer({
+  dest: "images"
+});
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
 // maintenence message
 // app.use((req, res, next) => {
 //   res.status(503).send("Site is currently down. Check back soon!");
