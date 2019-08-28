6c0e4d897ef3c84ea13535815be011a411563c1b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 17:31:03 -0500

    16-139: writing our own tests

--- udemy/complete-nodejs-dev/task-manager/tests/math.test.js
+++ udemy/complete-nodejs-dev/task-manager/tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require("../src/math");
+const {
+  calculateTip,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius
+} = require("../src/math");
 
 test("should calculate total with tip", () => {
   const total = calculateTip(10, 0.3);
@@ -8,6 +12,16 @@ test("should calculate total with tip", () => {
 test("should calculate total with default tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
+});
+
+test("should convert 32 F to 0 C", () => {
+  const temp = fahrenheitToCelsius(32);
+  expect(temp).toBe(0);
+});
+
+test("should convert 0 C to 32 F", () => {
+  const temp = celsiusToFahrenheit(0);
+  expect(temp).toBe(32);
 });
 
 // Why test?
