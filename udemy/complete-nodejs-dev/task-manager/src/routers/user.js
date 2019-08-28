9051b977bd816314e146ebb62441d0a125039034
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 07:33:19 -0500

    14-125: adding user avatar upload validation

--- udemy/complete-nodejs-dev/task-manager/src/routers/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/routers/user.js
@@ -90,7 +90,16 @@ router.post("/users/logoutAll", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: "avatars"
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload a[n] image of jpg, jpeg or png"));
+    }
+    cb(undefined, true);
+  }
 });
 
 router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
