eecdb8cadec61c14b2c6a21993c8a11a0ea8e0c8
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 17:13:49 -0500

    16-137: adding jest

---
+++ udemy/complete-nodejs-dev/task-manager/tests/math.test.js
@@ -0,0 +1,17 @@
+test("Hello world!", () => {
+  // export
+});
+
+test("This should fail", () => {
+  throw new Error("Failure");
+});
+
+// Why test?
+//
+//  - Saves time
+//  - Creates reliable software
+//  - Gives flexibility to developers
+//      - refactoring
+//      - collaborating
+//      - profiling
+//  - Peace of mind
