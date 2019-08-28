ffe11b841ee878c7b89088d0c1f89c36f30a3ce4
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 10:07:05 -0500

    15-132: send welcome and cancellation emails

--- udemy/complete-nodejs-dev/task-manager/src/routers/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/routers/user.js
@@ -4,16 +4,21 @@ const sharp = require("sharp");
 
 const User = require("../models/user");
 const auth = require("../middleware/auth");
-
+const {
+  sendWelcomeEmail,
+  sendCancellationEmail
+} = require("../emails/account");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (err) {
+    console.error(err);
     res.status(400).send(err);
   }
 });
@@ -49,8 +54,10 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
+    sendCancellationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (err) {
+    console.log(err);
     res.status(500).send(err);
   }
 });
