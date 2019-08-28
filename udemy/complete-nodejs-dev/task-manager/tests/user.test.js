9bfb9494808c7ec859bdce6fb2ade898686093c5
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 18:10:57 -0500

    16-143: setup and teardown

--- udemy/complete-nodejs-dev/task-manager/tests/user.test.js
+++ udemy/complete-nodejs-dev/task-manager/tests/user.test.js
@@ -1,7 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
 
-beforeEach(() => {});
+const userOne = {
+  name: "test user",
+  email: "testuser@example.com",
+  password: "56wat!!"
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("should signup a new user", async () => {
   await request(app)
@@ -12,4 +22,24 @@ test("should signup a new user", async () => {
       password: "MyPass777!"
     })
     .expect(201);
+});
+
+test("should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test("should not login non-existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "myfailingpw34!"
+    })
+    .expect(400);
 });
