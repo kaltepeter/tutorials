ec99240ba535731e6e491922440fadb8c488278b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 17:44:44 -0500

    16-140: async testing with jest

--- udemy/complete-nodejs-dev/task-manager/tests/math.test.js
+++ udemy/complete-nodejs-dev/task-manager/tests/math.test.js
@@ -1,7 +1,8 @@
 const {
   calculateTip,
   celsiusToFahrenheit,
-  fahrenheitToCelsius
+  fahrenheitToCelsius,
+  add
 } = require("../src/math");
 
 test("should calculate total with tip", () => {
@@ -22,6 +23,25 @@ test("should convert 32 F to 0 C", () => {
 test("should convert 0 C to 32 F", () => {
   const temp = celsiusToFahrenheit(0);
   expect(temp).toBe(32);
+});
+
+test.skip("async test demo", done => {
+  setTimeout(() => {
+    expect(1).toBe(2);
+    done();
+  }, 2000);
+});
+
+test("should add two numbers", done => {
+  add(2, 3).then(sum => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+test("should add two numbers async/await", async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
 });
 
 // Why test?
