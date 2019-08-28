397d41e0b11abbdcde65376c171de1ba3f0fd3ff
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 08:09:00 -0500

    14-129: adjust images

--- udemy/complete-nodejs-dev/task-manager/src/routers/user.js
+++ udemy/complete-nodejs-dev/task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 
 const User = require("../models/user");
 const auth = require("../middleware/auth");
@@ -106,7 +107,12 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -128,7 +134,7 @@ router.get("/users/:id/avatar", async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error();
     }
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (err) {
     console.error(err);
