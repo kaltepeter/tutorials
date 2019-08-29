b395d9c183b2ca00e08a2f2f8638f5d945acd782
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 19:11:43 -0500

    16-147: wrap up user tests

--- udemy/complete-nodejs-dev/task-manager/tests/user.test.js
+++ udemy/complete-nodejs-dev/task-manager/tests/user.test.js
@@ -106,3 +106,36 @@ test("should not delete account for unauthenticated user", async () => {
     .send()
     .expect(401);
 });
+
+test("should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("should update valid user fields", async () => {
+  const response = await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Andrew2"
+    })
+    .expect(200);
+
+  expect(response.body.name).toBe("Andrew2");
+});
+
+test("should not update invalid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: "Pittsberg"
+    })
+    .expect(400);
+});
