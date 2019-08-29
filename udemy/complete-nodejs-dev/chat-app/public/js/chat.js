f188ce88af54c1a35b560d5500ec1be5563c86d9
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-29 01:06:47 -0500

    17-160: form and button states

--- udemy/complete-nodejs-dev/chat-app/public/js/chat.js
+++ udemy/complete-nodejs-dev/chat-app/public/js/chat.js
@@ -8,13 +8,23 @@ socket.on("message", message => {
   console.log(message);
 });
 
-const messageForm = document.querySelector("#messageForm");
+const $messageForm = document.querySelector("#message-form");
+const $messageInput = $messageForm.querySelector('input[type="text"]');
+const $messageFormButton = $messageForm.querySelector('input[type="submit"]');
+const $sendLocationButton = document.querySelector("#send-location");
 
-messageForm.addEventListener("submit", e => {
+$messageForm.addEventListener("submit", e => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const formData = new FormData(e.target);
   const message = formData.get("message");
   socket.emit("sendMessage", message, error => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageInput.value = "";
+    $messageInput.focus();
+
     if (error) {
       console.error(error);
     }
@@ -26,10 +36,14 @@ document.querySelector("#send-location").addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     socket.emit("sendLocation", { latitude, longitude }, () => {
       console.log(`Location shared!`);
+      $sendLocationButton.removeAttribute("disabled");
     });
   });
 });
