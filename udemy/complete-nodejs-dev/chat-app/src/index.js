f951396728f2e0519f3ab98fb2ddea57b0976331
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-28 22:44:01 -0500

    17-154: adding socket.io

--- udemy/complete-nodejs-dev/chat-app/src/index.js
+++ udemy/complete-nodejs-dev/chat-app/src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "..", "public");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("New websocket connection");
+});
+
+server.listen(port, () => {
   console.log(`Chat app started on: http://localhost:${port}`);
 });
