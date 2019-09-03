656ddf29ba6cb1745ffe7d4dc031c997f6c5931b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-09-03 12:09:47 -0500

    intro to testing

--- pluralsight/pluralsight-react-fundamentals/authorquiz/src/AuthorQuiz.test.js
+++ pluralsight/pluralsight-react-fundamentals/authorquiz/src/AuthorQuiz.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import AuthorQuiz from "./AuthorQuiz";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
