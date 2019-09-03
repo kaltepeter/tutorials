bd94d022a78468ffac8b7a2f40a29e2f48d1a3fd
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-09-03 13:09:29 -0500

    jsx overview

--- pluralsight/pluralsight-react-fundamentals/authorquiz/src/AuthorQuiz.js
+++ pluralsight/pluralsight-react-fundamentals/authorquiz/src/AuthorQuiz.js
@@ -1,8 +1,67 @@
 import React from "react";
 import "./App.css";
 
-function AuthorQuiz() {
-  return <div>Author Quiz</div>;
-}
+const Hero = () => {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book writter by the author shown</p>
+      </div>
+    </div>
+  );
+};
+
+const Book = ({ title }) => {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+};
+
+const Turn = ({ author, books }) => {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Continue = () => {
+  return <div />;
+};
+
+const Footer = () => {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from{" "}
+          <a href="http://commons.wikimedia.org/wiki/Main">Wikimedia Commons</a>{" "}
+          and are in the public domain.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const AuthorQuiz = ({ turnData }) => {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
+    </div>
+  );
+};
 
 export default AuthorQuiz;
--- pluralsight/pluralsight-react-fundamentals/authorquiz/src/index.js
+++ pluralsight/pluralsight-react-fundamentals/authorquiz/src/index.js
@@ -1,10 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shuffle, sample } from "underscore";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import AuthorQuiz from "./AuthorQuiz";
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById("root"));
+const authorData = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"]
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.png",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"]
+  },
+  {
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Harry Potter and the Sorcerers Stone"]
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Shining", "IT"]
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"]
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"]
+  }
+];
+
+const getTurnData = authors => {
+  const allBooks = authors.reduce((p, c, i) => {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer))
+  };
+};
+
+const state = {
+  turnData: getTurnData(authorData)
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
