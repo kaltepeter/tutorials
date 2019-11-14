a462b43c185d33c9d781198132af7b4d048c13dd
kaltepeter <kayla@kaylaaltepeter.com>
2019-11-14 11:52:19 -0600

    more samples

--- frontendmasters/fp-class/curry.js
+++ frontendmasters/fp-class/curry.js
@@ -1,4 +1,4 @@
-const {curry} = 'ramda';
+const {curry} = require('ramda');
 
 const add = (x,y) => x + y;
 
@@ -8,7 +8,7 @@ const fromPair = f => (x,y) => f([x,y]);
 
 const flip = f => (y,x) => f(x,y);
 
-const curry = f => x => y => f(x,y);
+// const curry = f => x => y => f(x,y);
 
 const uncurry = f => (x,y) => f(x)(y);
 
@@ -16,7 +16,7 @@ const modulo = curry((x,y) => y % x);
 
 const isOdd = modulo(2);
 
-console.log(isOdd());
+console.log(isOdd);
 
 let result = fromPair(toPair(add))(1,2);
 console.log(result);
@@ -34,3 +34,29 @@ const filter = curry((f, xs) => xs.filter(f));
 
 const getOdds = filter(isOdd);
 console.log(getOdds([1,2,3,4,5,6]));
+
+const replace = curry((regex, replacement, str) => str.replace(regex, replacement));
+const replaceVowels = replace(/[AEIOU]/ig, '!');
+
+console.log(replaceVowels('hello'));
+
+const toUpper = str => str.toUpperCase();
+const exclaim = str => `${str}!`;
+const first = xs => xs[0];
+
+const compose = (f,g) => x => f(g(x));
+// const compose = (f,g) => x => exclaim(toUpper(x));
+// f . g = x => f(g(x));
+
+const shout = compose(exclaim, toUpper);
+// const shoutLetter = compose(first, compose(exclaim, toUpper)); // not working
+const loudFirst = compose(toUpper, first);
+const shoutLetter = compose(exclaim, loudFirst);
+
+console.log(shout('tears'));
+console.log(shoutLetter('tears'));
+
+const log = curry((tag,x) => (console.log(tag,x), x));
+
+const shout2 = compose(exclaim, log('here'), toUpper);
+console.log(shout2('yo'));
