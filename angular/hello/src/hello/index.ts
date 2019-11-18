858415ec513e190cdb3d519383913770cb913bc0
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-11-18 13:31:23 -0600

    7: templates

--- angular/hello/src/hello/index.ts
+++ angular/hello/src/hello/index.ts
@@ -1,12 +1,34 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  apply,
+  template,
+  mergeWith
+} from "@angular-devkit/schematics";
 import { Schema } from "./schema";
+import { strings } from "@angular-devkit/core";
+
+const addExclamation = (value: string): string => {
+  return `${value}!`;
+};
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function hello(_options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const { name } = _options;
-    tree.create("hello.js", `console.log('Hello ${name}!');`);
-    return tree;
+  return (_: Tree, _context: SchematicContext) => {
+    const sourceTemplates = url("./files");
+
+    const sourceParametrizedTemplate = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+        addExclamation
+      })
+    ]);
+
+    return mergeWith(sourceParametrizedTemplate);
+    // return mergeWith(sourceParametrizedTemplate)(tree, _context);
   };
 }
