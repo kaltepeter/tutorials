aca326f4dbdc971a926016715c5b9bffb4f9031d
kaltepeter <kayla@kaylaaltepeter.com>
2019-11-27 22:44:31 -0600

    4. services

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { of, Observable } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add(`HeroService: fetched heroes`);
     return of(HEROES);
   }
 }
