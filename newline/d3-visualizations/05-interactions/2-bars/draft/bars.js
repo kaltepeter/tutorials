c25e7fb356905e89d6e720bca3f8bc7283c5055c
kaltepeter <kayla@kaylaaltepeter.com>
2019-12-01 18:29:18 -0600

    bar chart interactions

--- newline/d3-visualizations/05-interactions/2-bars/draft/bars.js
+++ newline/d3-visualizations/05-interactions/2-bars/draft/bars.js
@@ -1,12 +1,11 @@
 async function drawBars() {
-
   // 1. Access data
 
-  const dataset = await d3.json("./../../../my_weather_data.json")
+  const dataset = await d3.json("./../../../my_weather_data.json");
 
   // 2. Create chart dimensions
 
-  const width = 600
+  const width = 600;
   let dimensions = {
     width: width,
     height: width * 0.6,
@@ -14,106 +13,142 @@ async function drawBars() {
       top: 30,
       right: 10,
       bottom: 50,
-      left: 50,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+      left: 50
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
-  const wrapper = d3.select("#wrapper")
+  const wrapper = d3
+    .select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
-  const bounds = wrapper.append("g")
-      .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 
   // init static elements
-  bounds.append("g")
-      .attr("class", "bins")
-  bounds.append("line")
-      .attr("class", "mean")
-  bounds.append("g")
-      .attr("class", "x-axis")
-      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+  bounds.append("g").attr("class", "bins");
+  bounds.append("line").attr("class", "mean");
+  bounds
+    .append("g")
+    .attr("class", "x-axis")
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`)
     .append("text")
-      .attr("class", "x-axis-label")
+    .attr("class", "x-axis-label");
 
-  const metricAccessor = d => d.humidity
-  const yAccessor = d => d.length
+  const metricAccessor = d => d.humidity;
+  const yAccessor = d => d.length;
+
+  const formatHumidity = d3.format(".2f");
 
   // 4. Create scales
 
-  const xScale = d3.scaleLinear()
+  const xScale = d3
+    .scaleLinear()
     .domain(d3.extent(dataset, metricAccessor))
     .range([0, dimensions.boundedWidth])
-    .nice()
+    .nice();
 
-  const binsGenerator = d3.histogram()
+  const binsGenerator = d3
+    .histogram()
     .domain(xScale.domain())
     .value(metricAccessor)
-    .thresholds(12)
+    .thresholds(12);
 
-  const bins = binsGenerator(dataset)
+  const bins = binsGenerator(dataset);
 
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .domain([0, d3.max(bins, yAccessor)])
     .range([dimensions.boundedHeight, 0])
-    .nice()
+    .nice();
 
   // 5. Draw data
 
-  const barPadding = 1
+  const barPadding = 1;
 
-  let binGroups = bounds.select(".bins")
+  const tooltip = d3.select("#tooltip");
+
+  function onMouseEnter(datum) {
+    tooltip.select("#count").text(yAccessor(datum));
+    tooltip
+      .select("#range")
+      .text([formatHumidity(datum.x0), formatHumidity(datum.x1)].join(" - "));
+    const x =
+      xScale(datum.x0) +
+      (xScale(datum.x1) - xScale(datum.x0)) / 2 +
+      dimensions.margin.left;
+    const y = yScale(yAccessor(datum)) + dimensions.margin.top;
+    tooltip.style(
+      "transform",
+      `translate(calc( -50% + ${x}px), calc( -100% + ${y}px ))`
+    );
+    tooltip.style("opacity", 1);
+  }
+
+  function onMouseLeave() {
+    tooltip.style("opacity", 0);
+  }
+
+  let binGroups = bounds
+    .select(".bins")
     .selectAll(".bin")
-    .data(bins)
+    .data(bins);
 
-  binGroups.exit()
-      .remove()
+  binGroups.exit().remove();
 
-  const newBinGroups = binGroups.enter().append("g")
-      .attr("class", "bin")
+  const newBinGroups = binGroups
+    .enter()
+    .append("g")
+    .attr("class", "bin");
 
-  newBinGroups.append("rect")
-  newBinGroups.append("text")
+  newBinGroups.append("rect");
+  newBinGroups.append("text");
 
   // update binGroups to include new points
-  binGroups = newBinGroups.merge(binGroups)
+  binGroups = newBinGroups.merge(binGroups);
 
-  const barRects = binGroups.select("rect")
-      .attr("x", d => xScale(d.x0) + barPadding)
-      .attr("y", d => yScale(yAccessor(d)))
-      .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
-      .attr("width", d => d3.max([
-        0,
-        xScale(d.x1) - xScale(d.x0) - barPadding
-      ]))
+  const barRects = binGroups
+    .select("rect")
+    .attr("x", d => xScale(d.x0) + barPadding)
+    .attr("y", d => yScale(yAccessor(d)))
+    .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
+    .attr("width", d => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]));
 
-  const mean = d3.mean(dataset, metricAccessor)
+  const mean = d3.mean(dataset, metricAccessor);
 
-  const meanLine = bounds.selectAll(".mean")
-      .attr("x1", xScale(mean))
-      .attr("x2", xScale(mean))
-      .attr("y1", -20)
-      .attr("y2", dimensions.boundedHeight)
+  const meanLine = bounds
+    .selectAll(".mean")
+    .attr("x1", xScale(mean))
+    .attr("x2", xScale(mean))
+    .attr("y1", -20)
+    .attr("y2", dimensions.boundedHeight);
 
   // draw axes
-  const xAxisGenerator = d3.axisBottom()
-    .scale(xScale)
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
 
-  const xAxis = bounds.select(".x-axis")
-    .call(xAxisGenerator)
+  const xAxis = bounds.select(".x-axis").call(xAxisGenerator);
 
-
-  const xAxisLabel = xAxis.select(".x-axis-label")
-      .attr("x", dimensions.boundedWidth / 2)
-      .attr("y", dimensions.margin.bottom - 10)
-      .text("Humidity")
+  const xAxisLabel = xAxis
+    .select(".x-axis-label")
+    .attr("x", dimensions.boundedWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .text("Humidity");
 
   // 7. Set up interactions
-
+  binGroups
+    .select("rect")
+    .on("mouseenter", onMouseEnter)
+    .on("mouseleave", onMouseLeave);
 }
-drawBars()
+drawBars();
