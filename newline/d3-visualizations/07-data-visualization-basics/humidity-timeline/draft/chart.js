2efc8df2bea483b43d869f2fff9d5b6ffb37ac58
kaltepeter <kayla@kaylaaltepeter.com>
2019-12-02 23:29:29 -0600

    fixing chart

--- newline/d3-visualizations/07-data-visualization-basics/humidity-timeline/draft/chart.js
+++ newline/d3-visualizations/07-data-visualization-basics/humidity-timeline/draft/chart.js
@@ -1,15 +1,19 @@
 async function drawLineChart() {
-
   // 1. Access data
 
-  let dataset = await d3.json("./../../../my_weather_data.json")
+  let dataset = await d3.json("./../../../my_weather_data.json");
 
-  const yAccessor = d => d.humidity
-  const dateParser = d3.timeParse("%Y-%m-%d")
-  const dateFormatter = d3.timeFormat("%Y-%m-%d")
-  const xAccessor = d => dateParser(d.date)
-  dataset = dataset.sort((a,b) => xAccessor(a) - xAccessor(b))
-  const weeks = d3.timeWeeks(xAccessor(dataset[0]), xAccessor(dataset[dataset.length - 1]))
+  const yAccessor = d => d.humidity;
+  const dateParser = d3.timeParse("%Y-%m-%d");
+  const dateFormatter = d3.timeFormat("%Y-%m-%d");
+  const xAccessor = d => dateParser(d.date);
+  dataset = dataset.sort((a, b) => xAccessor(a) - xAccessor(b));
+
+  const downsampledData = downsampleData(dataset, xAccessor, yAccessor);
+  const weeks = d3.timeWeeks(
+    xAccessor(dataset[0]),
+    xAccessor(dataset[dataset.length - 1])
+  );
 
   // 2. Create chart dimensions
 
@@ -20,102 +24,181 @@ async function drawLineChart() {
       top: 15,
       right: 15,
       bottom: 40,
-      left: 60,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+      left: 60
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
-  const wrapper = d3.select("#wrapper")
+  const wrapper = d3
+    .select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
-  const bounds = wrapper.append("g")
-      .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 
-  const defs = bounds.append("defs")
+  const defs = bounds.append("defs");
 
   // 4. Create scales
 
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .domain(d3.extent(dataset, yAccessor))
     .range([dimensions.boundedHeight, 0])
-    .nice()
+    .nice();
 
-  const xScale = d3.scaleTime()
+  const xScale = d3
+    .scaleTime()
     .domain(d3.extent(dataset, xAccessor))
-    .range([0, dimensions.boundedWidth])
+    .range([0, dimensions.boundedWidth]);
 
   // create grid marks
-  const yAxisGeneratorGridMarks = d3.axisLeft(yScale)
-      .ticks()
-      .tickSize(-dimensions.boundedWidth)
-      .tickFormat("")
+  const yAxisGeneratorGridMarks = d3
+    .axisLeft(yScale)
+    .ticks()
+    .tickSize(-dimensions.boundedWidth)
+    .tickFormat("");
 
-  const yAxisGridMarks = bounds.append("g")
-      .attr("class", "y-axis-grid-marks")
-    .call(yAxisGeneratorGridMarks)
+  const yAxisGridMarks = bounds
+    .append("g")
+    .attr("class", "y-axis-grid-marks")
+    .call(yAxisGeneratorGridMarks);
 
   // 5. Draw data
+  const seasonBoundaries = ["3-20", "6-21", "9-21", "12-21"];
+  const seasonNames = ["Spring, Summer", "Fall", "Winter"];
 
-  const dots = bounds.selectAll(".dot")
+  let seasonsData = [];
+  const startDate = xAccessor(dataset[0]);
+  const endDate = xAccessor(dataset[dataset.length - 1]);
+  const years = d3.timeYears(d3.timeMonth.offset(startDate, -13), endDate);
+  years.forEach(yearDate => {
+    const year = +d3.timeFormat("%Y")(yearDate);
+    seasonBoundaries.forEach((boundary, index) => {
+      const seasonStart = dateParser(`${year}-${boundary}`);
+      const seasonEnd = seasonBoundaries[index + 1]
+        ? dateParser(`${year}-${seasonBoundaries[index + 1]}`)
+        : dateParser(`${year + 1}-${seasonBoundaries[0]}`);
+      const boundaryStart = d3.max([startDate, seasonStart]);
+      const boundaryEnd = d3.min([endDate, seasonEnd]);
+      const days = dataset.filter(
+        d => xAccessor(d) > boundaryStart && xAccessor(d) <= boundaryEnd
+      );
+      if (!days.length) return;
+      seasonsData.push({
+        start: boundaryStart,
+        end: boundaryEnd,
+        name: seasonNames[index],
+        mean: d3.mean(days, yAccessor)
+      });
+    });
+  });
+
+  const seasonOffset = 10;
+  const seasons = bounds
+    .selectAll(".season")
+    .data(seasonsData)
+    .enter()
+    .append("rect")
+    .attr("x", d => xScale(d.start))
+    .attr("width", d => xScale(d.end) - xScale(d.start))
+    .attr("y", seasonOffset)
+    .attr("height", dimensions.boundedHeight - seasonOffset)
+    .attr("class", d => `season ${d.name}`);
+
+  const dots = bounds
+    .selectAll(".dot")
     .data(dataset)
-    .enter().append("circle")
-      .attr("cx", d => xScale(xAccessor(d)))
-      .attr("cy", d => yScale(yAccessor(d)))
-      .attr("r", 2)
-      .attr("class", "dot")
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(xAccessor(d)))
+    .attr("cy", d => yScale(yAccessor(d)))
+    .attr("r", 2)
+    .attr("class", "dot");
 
-  const lineGenerator = d3.area()
+  const lineGenerator = d3
+    .area()
     .x(d => xScale(xAccessor(d)))
     .y(d => yScale(yAccessor(d)))
-    .curve(d3.curveBasis)
+    .curve(d3.curveBasis);
 
-  const line = bounds.append("path")
-      .attr("class", "line")
-      .attr("d", lineGenerator(dataset))
-
+  const line = bounds
+    .append("path")
+    .attr("class", "line")
+    .attr("d", lineGenerator(downsampledData));
 
   // 6. Draw peripherals
+  const seasonMeans = bounds
+    .selectAll(".season-mean")
+    .data(seasonsData)
+    .enter()
+    .append("line")
+    .attr("x1", d => xScale(d.start))
+    .attr("x2", d => xScale(d.end))
+    .attr("y1", d => yScale(d.mean))
+    .attr("y2", d => yScale(d.mean))
+    .attr("class", "season-mean");
+  const seasonMeanLabel = bounds
+    .append("text")
+    .attr("x", -15)
+    .attr("y", yScale(seasonsData[0].mean))
+    .attr("class", "season-mean-label")
+    .text("Season mean");
 
-  const yAxisGenerator = d3.axisLeft()
+  const seasonLabels = bounds
+    .selectAll(".season-label")
+    .data(seasonsData)
+    .enter()
+    .append("text")
+    .filter(d => xScale(d.end) - xScale(d.start) > 60)
+    .attr("x", d => xScale(d.start) + (xScale(d.end) - xScale(d.start)) / 2)
+    .attr("y", dimensions.boundedHeight + 30)
+    .text(d => d.name)
+    .attr("class", "season-label");
+
+  const yAxisGenerator = d3
+    .axisLeft()
     .scale(yScale)
-    .ticks()
+    .ticks(3);
 
-  const yAxis = bounds.append("g")
-      .attr("class", "y-axis")
-    .call(yAxisGenerator)
+  const yAxis = bounds
+    .append("g")
+    .attr("class", "y-axis")
+    .call(yAxisGenerator);
 
-  const yAxisLabel = yAxis.append("text")
-      .attr("y", -dimensions.margin.left + 10)
-      .attr("x", -dimensions.boundedHeight / 2)
-      .attr("class", "y-axis-label")
-      .text("relative humidity")
-
-  const xAxisGenerator = d3.axisBottom()
-    .scale(xScale)
-    .ticks()
-
-  const xAxis = bounds.append("g")
-      .attr("class", "x-axis")
-      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
-    .call(xAxisGenerator)
+  const yAxisLabel = yAxis
+    .append("text")
+    .attr("y", -dimensions.margin.left + 10)
+    .attr("x", -dimensions.boundedHeight / 2)
+    .attr("class", "y-axis-label")
+    .text("relative humidity");
 }
-drawLineChart()
-
+drawLineChart();
 
 function downsampleData(data, xAccessor, yAccessor) {
-  const weeks = d3.timeWeeks(xAccessor(data[0]), xAccessor(data[data.length - 1]))
+  const weeks = d3.timeWeeks(
+    xAccessor(data[0]),
+    xAccessor(data[data.length - 1])
+  );
 
   return weeks.map((week, index) => {
-    const weekEnd = weeks[index + 1] || new Date()
-    const days = data.filter(d => xAccessor(d) > week && xAccessor(d) <= weekEnd)
+    const weekEnd = weeks[index + 1] || new Date();
+    const days = data.filter(
+      d => xAccessor(d) > week && xAccessor(d) <= weekEnd
+    );
     return {
       date: d3.timeFormat("%Y-%m-%d")(week),
-      humidity: d3.mean(days, yAccessor),
-    }
-  })
+      humidity: d3.mean(days, yAccessor)
+    };
+  });
 }
