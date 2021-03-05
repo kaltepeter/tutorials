8e6805a434f83d0bf68c05f9b43803c42ab36084
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-03-05 14:21:46 -0600

    --wip-- [skip ci]

--- pluralsight/pluralsight-gatsby/blog/src/pages/404.js
+++ pluralsight/pluralsight-gatsby/blog/src/pages/404.js
@@ -1,8 +1,13 @@
 import React from "react"
+import styles from "./404.module.scss"
 
 export default () => {
-  return <div>
-    <h1>Page not found</h1>
-    <p>The page you are looking for does not exist.</p>
-  </div>
+  return (
+    <div>
+      <h1 className={styles.header}>Page not found</h1>
+      <p className={styles.errorMessage}>
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  )
 }
