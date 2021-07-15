69a51cc553b1c9b87bf50fa4a9f16a116a72c5e1
Kayla Altepeter <kayla.altepeter@datasite.com>
2021-07-15 09:56:08 -0500

    feat(module-2): build components

--- pluralsight/pluralsight-desigining-react-components/pages/index.js
+++ pluralsight/pluralsight-desigining-react-components/pages/index.js
@@ -1,5 +1,17 @@
-const indexPage = () => {
-    return (<div>Hello from pluralsight</div>);
-}
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker";
 
-export default indexPage;
+
+const IndexPage = () => {
+    return (
+      <div className="container speakers-list">
+        <div className="row">
+          {data.map((speaker) => {
+            return <Speaker key={speaker.id} speaker={speaker} />;
+          })}
+        </div>
+      </div>
+    );
+};
+
+export default IndexPage;
---
+++ pluralsight/pluralsight-desigining-react-components/src/components/Speaker.js
@@ -0,0 +1,63 @@
+const Session = ({title, room}) => {
+    return (          
+    <span className="session w-100">
+      {title} <strong>Room: {room.name}</strong>
+    </span>
+    )
+  }
+  
+  const Sessions = ({sessions}) => {
+    return (
+      <div className="sessionBox card h-250">
+      <Session {...sessions[0]} />
+    </div>
+    )
+  }
+  
+  const SpeakerImage = ({id, first, last}) => {
+    return (
+      <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+        <img className="contain-fit" src={`/images/speaker-${id}.jpg`} width="300" alt={`${first} ${last}`} />
+      </div>
+    )
+  }
+  
+  const SpeakerDemographics = ({first, last, bio, company, twitterHandle, favorite}) => {
+    return (
+      <div className="speaker-info">
+      <div className="d-flex justify-content-between mb-3">
+        <h3 className="text-truncate w-200">{first} {last}</h3>
+      </div>
+      <div>
+        <p className="card-description">
+            {bio}
+        </p>
+        <div className="social d-flex flex-row mt-4">
+            <div className="company">
+                <h5>Company</h5>
+                <h6>{company}</h6>
+            </div>
+            <div className="twitter">
+                <h5>twitter</h5>
+                <h6>{twitterHandle}</h6>
+            </div>
+        </div>
+      </div>
+    </div>
+    );
+  }
+  
+  const Speaker = ({speaker}) => {
+      const {sessions,id, first, last} = speaker;
+    return (
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 cl-xs-12">
+        <div className="card card-height p-4 mt-4">
+          <SpeakerImage id={id} first={first} last={last} />
+          <SpeakerDemographics {...speaker} />
+        </div>
+        <Sessions sessions={sessions} />
+      </div>
+    );
+  }
+
+  export default Speaker;
