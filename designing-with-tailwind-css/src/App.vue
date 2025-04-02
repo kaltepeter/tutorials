df4e2b09df18e99fa2f1132c296bd877b39017dc
Kayla Altepeter <kayla@kaylaaltepeter.com>
2025-04-02 17:33:16 -0500

    feat(tailwind): designing with tailwind css (#487)
    Squashed commit of the following:
    
    commit 6671b9e8e38252a6740b3e1b17e545e167c512b3
    Author: kaltepeter <kayla.altepeter@datasite.com>
    Date:   Sun Sep 10 16:19:14 2023 -0500
    
        feat(dropdown): adding
    
    commit d9f6f3569a73bdebb711ae9179d1dfcd5d3e10e4
    Author: kaltepeter <kayla.altepeter@datasite.com>
    Date:   Sun Sep 10 11:14:48 2023 -0500
    
        building image card
    
    commit 0c174ee20203fcabb866baa62de33165691a87f2
    Author: kaltepeter <kayla.altepeter@datasite.com>
    Date:   Tue Sep 5 13:30:02 2023 -0500
    
        feat(properties): adding properties card
    
    commit 96457a8b88b1ef8943380c04b9ac96b1af19b1c0
    Author: kaltepeter <kayla.altepeter@datasite.com>
    Date:   Tue Sep 5 12:16:51 2023 -0500
    
        chore(vue): convert to vue
    
    commit f95e5dae897a13dd0dcfaeece5fa4666b728ca99
    Author: kaltepeter <kayla.altepeter@datasite.com>
    Date:   Tue Sep 5 11:45:58 2023 -0500
    
        initial html

---
+++ designing-with-tailwind-css/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavBar />
+  <div id="app" class="antialiased text-gray-900">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style src="./assets/tailwind.css"></style>
